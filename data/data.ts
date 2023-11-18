d4bafab78b08f23f7975114f57ab4f983922a318
DababyIsCooI <96953140+DababyIsCooI@users.noreply.github.com>
2023-11-18 17:32:37 -0500

    Update data.ts

--- data/data.ts
+++ data/data.ts
@@ -1,25 +1,13 @@
 const data = {
-    sitename: "Comming Soon Template",
-    sitetagline: "Comming Soon Template Tagline",
+    sitename: "Comming Soon",
+    sitetagline: "Comming Soon",
     sitelogo: "",
-    title: "Comming Soon",
-    description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
+    title: "Coming Soon",
+    description: "We&apos;re currently working hard to bring you something great, and we can&apos;t wait to share it with you. Benny Development is putting the finishing touches on Chill-Eze that we think you&apos;ll love 😍.",
     newsletterheading: "Stay tuned for our live updates!",
-    copyrightText: 'Copyright © 2023 | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://github.com/baikaresandip">Baikare Sandip</a>',
+    copyrightText: 'Copyright © 2023 | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://discord.gg/forgelogic">ForgeLogic Systems (Ben Copeland)</a>',
     socialIconsHeading: "Follow Us 📣",
     socialIcons: [
-        {
-            icon: "facebook",
-            link: "#",
-        },
-        {
-            icon: "twitter",
-            link: "#",
-        },
-        {
-            icon: "linkedIn",
-            link: "#",
-        },
     ],
 }
 
