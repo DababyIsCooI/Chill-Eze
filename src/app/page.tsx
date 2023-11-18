f0b61a0ea29a5cd3dedb5188954385b49a5465a2
DababyIsCooI <96953140+DababyIsCooI@users.noreply.github.com>
2023-11-18 17:49:02 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,15 +9,35 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   const currentYear = new Date().getFullYear()
   const {
+    sitename,
+    sitetagline,
     description,
     copyrightText,
     newsletterheading,
     sitelogo,
+    socialIcons,
+    socialIconsHeading,
     title,
   } = data
 
   return (
     <>
+      <main className="flex min-h-screen bg-slate-950 flex-col items-center justify-between p-5 lg:p-12">
+        <div className="z-10 w-full max-w-5xl items-center justify-between text-sm ">
+
+          <div className=" bottom-0 left-0 flex h-30 md:h-48 w-full items-end justify-center  lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <a
+              className="pointer-events-none flex place-items-center gap-2 p-4 lg:pointer-events-auto lg:p-0"
+              href="/"
+              rel="noopener noreferrer"
+            >
+              <div className='flex flex-col text-center'>
+
+              </div>
+            </a>
+          </div>
+        </div>
+
         <div className="relative flex flex-col  place-items-center ">
           <h2 className='text-center font-heading m-10 text-6xl sm:text-7xl lg:text-8xl leading-[5rem] sm:leading-[7rem] lg:leading-[7rem] font-bold '>
             <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>{title}</span>
@@ -28,7 +48,9 @@ export default function Home() {
           }}>
           </p>
         </div>
+        
         <footer className='text-slate-500 text-center'>
+
           <div className=''>
             <p className='' dangerouslySetInnerHTML={{
               __html: copyrightText
