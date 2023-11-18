4504872c85067cf6a8be55e030577981e2119fed
DababyIsCooI <96953140+DababyIsCooI@users.noreply.github.com>
2023-11-18 17:33:22 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,23 +22,6 @@ export default function Home() {
 
   return (
     <>
-      <main className="flex min-h-screen bg-slate-950 flex-col items-center justify-between p-5 lg:p-12">
-        <div className="z-10 w-full max-w-5xl items-center justify-between text-sm ">
-
-          <div className=" bottom-0 left-0 flex h-30 md:h-48 w-full items-end justify-center  lg:static lg:h-auto lg:w-auto lg:bg-none">
-            <a
-              className="pointer-events-none flex place-items-center gap-2 p-4 lg:pointer-events-auto lg:p-0"
-              href="/"
-              rel="noopener noreferrer"
-            >
-              <div className='flex flex-col text-center'>
-                <h1 className='text-2xl lgtext-4xl text-sky-400/100 font-semibold	'> {sitename} </h1>
-                <p className='text-2 font-medium m-2  text-slate-100'>{sitetagline}</p>
-              </div>
-            </a>
-          </div>
-        </div>
-
         <div className="relative flex flex-col  place-items-center ">
           <h2 className='text-center font-heading m-10 text-6xl sm:text-7xl lg:text-8xl leading-[5rem] sm:leading-[7rem] lg:leading-[7rem] font-bold '>
             <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>{title}</span>
