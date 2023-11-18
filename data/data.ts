08d91762eb29a77d6d02ecf1ca871dfcadf80e83
DababyIsCooI <96953140+DababyIsCooI@users.noreply.github.com>
2023-11-18 17:48:33 -0500

    Update data.ts

--- data/data.ts
+++ data/data.ts
@@ -5,7 +5,7 @@ const data = {
     title: "Coming Soon",
     description: "We&apos;re currently working hard to bring you something great, and we can&apos;t wait to share it with you. Benny Development is putting the finishing touches on Chill-Eze that we think you&apos;ll love 😍.",
     newsletterheading: "Stay tuned for our live updates!",
-    copyrightText: 'Copyright © 2023 | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://discord.gg/forgelogic">ForgeLogic Systems (Ben Copeland)</a>',
+    copyrightText: 'Copyright © 2023 | Design and Developed By <a target="_blank" class="no-underline md:underline" href="https://discord.gg/forgelogic">ForgeLogic Systems (Ben Copeland)</a>',
     socialIconsHeading: "Follow Us 📣",
     socialIcons: [
     ],
